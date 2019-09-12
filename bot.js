85d21e7edfcb1a4a30d064c05b191d7759be01d5
dylanbossie <dylanbossie@gmail.com>
2019-09-11 22:08:22 -0600

    Development on user commands to bot

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 var auth = require('./config/auth.json');
-var channels = require('./config/channels.json')
+var serverParams = require('./config/serverParameters.json')
+var tools = require('./lib/command-bot.js')
+
 // create a new Discord client
 const client = new Discord.Client();
 
@@ -11,7 +13,7 @@ client.once('ready', () => {
     console.log('Bot user metadata:')
     console.log(client.user)
     console.log('Valid channels:')
-    console.log(channels.validChannels)
+    console.log(serverParams.validChannels)
 });
 
 // login to Discord with your app's token
@@ -20,9 +22,17 @@ client.login(auth.token);
 // listen for a message
 client.on('message', message => {
     if (channels.validChannels.includes(message.channel.name)) {
+        msg = message.content
+        // TODO: implement check for a user command (prefix: !)
+        // add switch case with all desired commands, which then
+        // references command-bot to corresponding function
+        if (msg[0] == '!') {
+            args = msg.substring
+            switch
+        }
         console.log('New message! Message metadata:')
         console.log(message.author.id)
-        console.log(message.content)
+        console.log(msg)
 
         console.log('Reacting to message.')
         message.react('🤔')
