c04beef38d6106d3b7dd5cdb46a9bad2ff888306
dylanbossie <dylanbossie@gmail.com>
2019-09-11 21:47:04 -0600

    Add feature for reacting only in specific channels
    Added a /config/ folder to host channels.json. This config file
    holds a single field - validChannels, which stores the names
    of all channels the user desires for the bot to react to
    messages in.

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
-// require the discord.js module
 const Discord = require('discord.js');
-// require auth.json - config file containing authentication token for bot
-var auth = require('./auth.json');
+var auth = require('./config/auth.json');
+var channels = require('./config/channels.json')
 // create a new Discord client
 const client = new Discord.Client();
 
@@ -9,8 +8,10 @@ const client = new Discord.Client();
 // this event will only trigger one time after logging in
 client.once('ready', () => {
     console.log('Ready!');
-    console.log('Bot metadata:')
+    console.log('Bot user metadata:')
     console.log(client.user)
+    console.log('Valid channels:')
+    console.log(channels.validChannels)
 });
 
 // login to Discord with your app's token
@@ -18,10 +19,12 @@ client.login(auth.token);
 
 // listen for a message
 client.on('message', message => {
-    console.log('New message! Message metadata:')
-    console.log(message.author.id)
-    console.log(message.content)
+    if (channels.validChannels.includes(message.channel.name)) {
+        console.log('New message! Message metadata:')
+        console.log(message.author.id)
+        console.log(message.content)
 
-    console.log('Reacting to message.')
-    message.react('🤔')
+        console.log('Reacting to message.')
+        message.react('🤔')
+    }
 });
